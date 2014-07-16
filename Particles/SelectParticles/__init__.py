6e182b8cf8bb740f3d40c2243b530b345377e2fa
Alex Nie <anie@college.harvard.edu>
2014-07-16 04:03:37 -0400

    Updated get_distance_idx_ID for idts

--- Particles/SelectParticles/__init__.py
+++ Particles/SelectParticles/__init__.py
@@ -1,2 +1,3 @@
 from tdc_xp_data_with_selected     import *
 from tdc_xps_plotter_with_selected import *
+from select_particle import *
--- Particles/SelectParticles/select_particle.py
+++ Particles/SelectParticles/select_particle.py
@@ -5,3 +5,17 @@ Created on Wed Jul 16 03:45:08 2014
 @author: Alexander
 """
 
+class Select_Particle(object):
+    """
+    Class to keep track of all the different variables associated with selecting and tracking particles.
+    Can be expanded to include particle history.
+    """
+    def __init__(self, idts, ID, index, x, p):
+        self.idts = idts
+        self.ID = ID
+        self.index = index
+        self.x = x
+        self.p = p
+    def get_coordinates(self):
+        return (self.x, self.p)
+    
